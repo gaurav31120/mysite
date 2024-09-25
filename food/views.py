ba4996336f104caf99e6637ed25ff461f28fc1a7
Gaurav Kumar <gaurav31120@gmail.com>
2024-09-25 22:04:35 +0530

    11. importing templates in django (views)

--- food/views.py
+++ food/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
+from django.template import loader
 
 # Create your views here.
 # whatever name you have given to a function that same name is of views.
  
 def index(request):
     item_list = Item.objects.all()
-    return HttpResponse(item_list)
+    template = loader.get_template('food/index.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context,request))
 
 def item(request):
-    return HttpResponse('This is an item view')
+    return HttpResponse('<h1>This is an item view</h1>')
 
 def gaurav(request):
-    return HttpResponse('<h1>Gaurav Kumar</h2>')
+    return HttpResponse('<h1>Gaurav Kumar</h1>')
