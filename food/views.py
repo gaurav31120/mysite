ce0594de54bbf7285822caf07f59fe14dcded58a
Gaurav Kumar <gaurav31120@gmail.com>
2024-09-25 17:28:42 +0530

    10. importing models into view.py to show data

--- food/views.py
+++ food/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 
 # Create your views here.
 # whatever name you have given to a function that same name is of views.
  
 def index(request):
-    return HttpResponse('Hello World')
+    item_list = Item.objects.all()
+    return HttpResponse(item_list)
 
 def item(request):
     return HttpResponse('This is an item view')
