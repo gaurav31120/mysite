471425b9422d7dce0998f29f9723d618c2fd19b1
Gaurav Kumar <gaurav31120@gmail.com>
2024-09-25 13:51:45 +0530

    4. creating urls and attaching to views

--- food/views.py
+++ food/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-# whatever name you is given to a function that same name is of views.
+# whatever name you have given to a function that same name is of views.
  
 def index(request):
     return HttpResponse('Hello World')
