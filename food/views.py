450ffd404a241063675e8849cfa05e1e60b0c8f0
Gaurav Kumar <gaurav31120@gmail.com>
2024-09-25 22:20:08 +0530

    12. how to use templates and context in html

--- food/views.py
+++ food/views.py
@@ -10,9 +10,10 @@ def index(request):
     item_list = Item.objects.all()
     template = loader.get_template('food/index.html')
     context = {
-
+        'item_list':item_list,
     }
-    return HttpResponse(template.render(context,request))
+    # return HttpResponse(template.render(context,request))
+    return render(request, 'food/index.html',context)
 
 def item(request):
     return HttpResponse('<h1>This is an item view</h1>')
